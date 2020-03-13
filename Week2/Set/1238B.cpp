3e46c84caaf24731825b0c27cae4e05a39db3350
shauming1020 <50039782+shauming1020@users.noreply.github.com>
2020-03-13 22:17:52 +0800

    Update 1238B.cpp

--- Week2/Set/1238B.cpp
+++ Week2/Set/1238B.cpp
@@ -8,44 +8,45 @@ using namespace std;
 int main(void) {
 
     int q;
-    cin >> q;
+    scanf("%d", &q);
+
     int *fires = new int[q];
     for (int i = 0; i < q; i++) fires[i] = 0;
 
+    int n, r, c, tmp, dead, fire;
     for (int i = 0; i < q; i++) {
-        int n, r, c, tmp, dead = 0, fire = 0;
-            cin >> n >> r;
+            dead = 0, fire = 0;
 
-            set<int> S;
+            scanf("%d %d", &n, &r);
+
+            set<int> *S = new set<int>;
 
             for (int i = 0; i < n; i++) {
-                cin >> tmp;
-                S.insert(tmp);
+                scanf("%d", &tmp);
+                S->insert(tmp);
             }
 
             set<int>::iterator tail;
 
-            while (!S.empty()) {
-                tail = S.end(); tail--;
-                if (tail == S.begin()) {
+            while (true) {
+                tail = S->end(); tail--;
+                if (tail == S->begin()) {
                     if (*tail <= dead) break;
                     else {
                         fire++; break;
                     }
                 }
 
-                c = *tail;
-                fire++;
-                dead += r;
-                for (;tail!=S.begin();tail--)
-                    if (*tail <= dead || *tail == c) S.erase(tail);
+                c = *tail; fire++; dead += r;
+                for (;tail!=S->begin();tail--)
+                    if (*tail <= dead || *tail == c) tail = S->erase(tail);
 
             }
         fires[i] = fire;
     }
 
-    for (int i = 0; i < q; i++) cout << fires[i] << "\n";
+    for (int i = 0; i < q; i++) printf("%d", fires[i]);
 
-    system("pause");
     return 0;
 }
+
