96e6e5e285978c30cf89f2546c960403fb897879
shauming1020 <50039782+shauming1020@users.noreply.github.com>
2020-03-15 12:15:44 +0800

    Update 1238B.cpp

--- Week2/Set/1238B.cpp
+++ Week2/Set/1238B.cpp
@@ -1,52 +1,19 @@
 # https://codeforces.com/contest/1238/problem/B
-# 
+# AC
+
 #include <iostream>
 #include <set>
-
 using namespace std;
-
 int main(void) {
-
-    int q;
-    scanf("%d", &q);
-
-    int *fires = new int[q];
-    for (int i = 0; i < q; i++) fires[i] = 0;
-
-    int n, r, c, tmp, dead, fire;
-    for (int i = 0; i < q; i++) {
-            dead = 0, fire = 0;
-
-            scanf("%d %d", &n, &r);
-
-            set<int> *S = new set<int>;
-
-            for (int i = 0; i < n; i++) {
-                scanf("%d", &tmp);
-                S->insert(tmp);
-            }
-
-            set<int>::iterator tail;
-
-            while (true) {
-                tail = S->end(); tail--;
-                if (tail == S->begin()) {
-                    if (*tail <= dead) break;
-                    else {
-                        fire++; break;
-                    }
-                }
-
-                c = *tail; fire++; dead += r;
-                for (;tail!=S->begin();tail--)
-                    if (*tail <= dead || *tail == c) tail = S->erase(tail);
-
-            }
-        fires[i] = fire;
+    int q, n, r, c, tmp, dead, fire;
+    for (scanf("%d", &q); q--;) {
+        set<int> S;
+        for (scanf("%d %d", &n, &r); n--;)
+            scanf("%d", &tmp), S.insert(-tmp);
+        dead = 0, fire = 0;
+        for (auto index : S) fire += index + r * dead++ < 0;
+        printf("%d ", fire);
     }
-
-    for (int i = 0; i < q; i++) printf("%d\n", fires[i]);
-
     return 0;
 }
 
