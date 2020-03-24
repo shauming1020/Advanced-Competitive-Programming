3c1297a9d8f69024e9978a0373ea8718d04d4534
shauming1020 <50039782+shauming1020@users.noreply.github.com>
2020-03-24 14:12:09 +0800

    Create ProductTriplets_searching.cpp

---
+++ Week4/Search/ProductTriplets_searching.cpp
@@ -0,0 +1,40 @@
+# https://code.google.com/codejam/contest/5374486/dashboard#s=p0
+# AC
+
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int T, N;
+long long cnt, t, A[7010];
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> N;
+        for (int i = 0; i < N; i++) cin >> A[i];
+
+        // sort
+        sort(A, A+N);
+
+        // binary search, O(log(r-l))
+        cnt = 0;
+        for (int x = 0; x < N-1; x++) {
+            for (int y = x+1; y <N; y++) {
+                t = A[x] * A[y];
+                if (t||!A[j]) // t != 0 or A[j] != 0
+                    cnt += upper_bound(A+j+1, A+N, t) - lower_bound(A+j+1, A+N, t);
+                else cnt += upper_bound(A+i+1, A+j, 0) - lower_bound(A+i+1, A+j, 0);
+            }
+        }
+
+        cout << "Case #" << i+1 << ": " << cnt << endl;
+    }
+
+    return 0;
+}
